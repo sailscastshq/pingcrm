0a01ee413fb789e5193d0979aae7325f7aad01ad
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-05-18 20:16:43 +0100

    feat: add model and factory for contact

---
+++ api/models/Contact.js
@@ -0,0 +1,66 @@
+/**
+ * Contact.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    firstName: {
+      type: 'string',
+      columnName: 'first_name',
+      maxLength: 25
+    },
+    lastName: {
+      type: 'string',
+      columnName: 'last_name',
+      maxLength: 25
+    },
+    email: {
+      type: 'string',
+      maxLength: 50
+    },
+    phone: {
+      type: 'string',
+      maxLength: 50
+    },
+    address: {
+      type: 'string',
+      maxLength: 150
+    },
+    city: {
+      type: 'string',
+      maxLength: 50
+    },
+    region: {
+      type: 'string',
+      maxLength: 50
+    },
+    country: {
+      type: 'string',
+      maxLength: 2
+    },
+    postalCode: {
+      type: 'string',
+      maxLength: 25
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    account: {
+      model: 'account'
+    },
+    organization: {
+      model: 'organization'
+    }
+  }
+}
