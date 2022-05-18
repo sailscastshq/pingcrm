d60c673e240150d124df29d144807360c465ee8e
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-05-18 20:17:00 +0100

    fix: remove length from users

--- config/factories/user.js
+++ config/factories/user.js
@@ -2,8 +2,8 @@ const { fake } = require('captain-vane')
 
 module.exports.default = () => {
   return {
-    firstName: fake.randFirstName({ length: 25 }),
-    lastName: fake.randLastName({ length: 25 }),
+    firstName: fake.randFirstName(),
+    lastName: fake.randLastName(),
     email: fake.randEmail(),
     password: fake.randPassword(),
     photoPath: fake.randomAvatar()
