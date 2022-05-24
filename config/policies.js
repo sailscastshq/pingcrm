78b48c8cdb3ab2cfdc57670ef1522024c9205f78
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-05-24 11:21:21 +0100

    feat: update policies list

--- config/policies.js
+++ config/policies.js
@@ -9,14 +9,13 @@
  */
 
 module.exports.policies = {
-
   /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions, unless overridden.       *
-  * (`true` allows public access)                                            *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Default policy for all controllers and actions, unless overridden.       *
+   * (`true` allows public access)                                            *
+   *                                                                          *
+   ***************************************************************************/
 
-  // '*': true,
-
-};
+  '*': 'is-authenticated',
+  'user/*': 'is-guest'
+}
