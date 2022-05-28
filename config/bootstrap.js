957d1a3d4485e927a8d3c0c83a8c4a2a367095c3
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-05-28 18:36:54 +0100

    feat: bootstrap fake data

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,22 +9,21 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function() {
-
+module.exports.bootstrap = async function () {
   // By convention, this is a good place to set up fake data during development.
   //
   // For example:
   // ```
-  // // Set up fake development data (or if we already have some, avast)
-  // if (await User.count() > 0) {
-  //   return;
-  // }
-  //
-  // await User.createEach([
-  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
-  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
-  //   // etc.
-  // ]);
-  // ```
-
-};
+  // Set up fake development data (or if we already have some, avast)
+  if ((await User.count()) > 0) {
+    return
+  }
+  const account = await sails.helpers.vane('account')
+  await sails.helpers.vane('user', {
+    email: 'jacksparrow@blackpearl.com',
+    password: 'parley',
+    account: account.id,
+    owner: true
+  })
+  await sails.helpers.vane('user', { account: account.id }, { count: 5 })
+}
