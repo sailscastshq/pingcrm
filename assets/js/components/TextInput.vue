aefa0cb2456f4aeb96e5a6a6bd5105d4363d9b03
Kelvin Omereshone <kelvinomereshone@gmail.com>
2022-05-30 14:50:17 +0100

    chore: move template below scripts

--- assets/js/components/TextInput.vue
+++ assets/js/components/TextInput.vue
@@ -1,24 +1,3 @@
-<template>
-  <div :class="$attrs.class">
-    <label v-if="label" class="mb-2 block select-none text-gray-700" :for="id"
-      >{{ label }}:</label
-    >
-    <input
-      :id="id"
-      ref="input"
-      v-bind="{ ...$attrs, class: null }"
-      class="relative block w-full appearance-none rounded border bg-white p-2 text-left font-sans leading-normal text-gray-700 placeholder:text-gray-500 placeholder:opacity-100 focus:border-indigo-400 focus:outline-none focus:ring active:outline-none"
-      :class="
-        error ? ['border-red-500', 'focus:ring', 'focus:ring-red-200'] : []
-      "
-      :type="type"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-    />
-    <div v-if="error" class="mt-2 text-sm text-red-700">{{ error }}</div>
-  </div>
-</template>
-
 <script setup>
 import { v4 as uuid } from 'uuid'
 import { ref } from 'vue'
@@ -57,3 +36,24 @@ export default {
   inheritAttrs: false
 }
 </script>
+
+<template>
+  <div :class="$attrs.class">
+    <label v-if="label" class="mb-2 block select-none text-gray-700" :for="id"
+      >{{ label }}:</label
+    >
+    <input
+      :id="id"
+      ref="input"
+      v-bind="{ ...$attrs, class: null }"
+      class="relative block w-full appearance-none rounded border bg-white p-2 text-left font-sans leading-normal text-gray-700 placeholder:text-gray-500 placeholder:opacity-100 focus:border-indigo-400 focus:outline-none focus:ring active:outline-none"
+      :class="
+        error ? ['border-red-500', 'focus:ring', 'focus:ring-red-200'] : []
+      "
+      :type="type"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <div v-if="error" class="mt-2 text-sm text-red-700">{{ error }}</div>
+  </div>
+</template>
